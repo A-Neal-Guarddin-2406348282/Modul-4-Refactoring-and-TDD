5a8b99a09599a2575da45f2afc0bb649852cdcae
Neal Guarddin <guarddin29@gmail.com>
2026-03-09 22:52:58 +0700

    [GREEN] Implement Payment model

--- src/main/java/model/Payment.java
+++ src/main/java/model/Payment.java
@@ -1,24 +1,24 @@
-//package model;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import java.util.Map;
-//
-//@Getter
-//@Setter
-//public class Payment {
-//    private String id;
-//    private Order order;
-//    private String method;
-//    private String status;
-//    private Map<String, String> paymentData;
-//
-//    public Payment(String id, Order order, String method, String status, Map<String, String> paymentData) {
-//        this.id = id;
-//        this.order = order;
-//        this.method = method;
-//        this.status = status;
-//        this.paymentData = paymentData;
-//    }
-//}
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+public class Payment {
+    private String id;
+    private Order order;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+
+    public Payment(String id, Order order, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
+    }
+}
